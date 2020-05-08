141e560f038e3a68e4f786af30f656ec95a477c1
imugi <qweasdzd@hotmail.com>
2020-05-08 22:21:05 +0900

    Xamarin - 검색 / 검색화면

---
+++ DoitDoit/DoitDoit/DoitDoit/DoitDoit/Models/FoodList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace DoitDoit.Models {
+    class FoodList {
+        public ICommand AddFoodCommand => new Command(AddFood);
+
+        public ICommand RemoveFoodCommand => new Command(RemoveFood);
+
+        public ObservableCollection<string> Foods { get; set; }
+
+        public string FoodName { get; set; }
+        public string SelectedFood { get; set; }
+
+        public FoodList() {
+            Foods = new ObservableCollection<string>();
+
+            Foods.Add("가자미구이");
+            Foods.Add("현미밥");
+            Foods.Add("미역국");
+        }
+
+        public void AddFood() {
+            Foods.Add(FoodName);
+
+        }
+        public void RemoveFood() {
+            Foods.Remove(SelectedFood);
+        }
+    }
+}
---
+++ DoitDoit/DoitDoit/DoitDoit/DoitDoit/SearchFood.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DoitDoit {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SearchFood : ContentPage {
+        public SearchFood() {
+            InitializeComponent();
+        }
+
+        private void Register_Clicked(object sender, EventArgs e) {
+            Navigation.PushModalAsync(new MainPage());
+        }
+    }
+}
